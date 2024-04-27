b1cb0f2fb997b01f2f2086574f593808002ff068
M.Zeeshan <109668276+M-ZeeshanDev@users.noreply.github.com>
2024-04-27 13:34:45 +0500

    TenantService Changes update
    TenantService Changes update

---
+++ Tenant/TenantService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tenant
+{
+    public record Tenant(int Id, string Host, bool IsActive, string ThemeName);
+
+    public class TenantService
+    {
+        private readonly List<Tenant> _tenants;
+
+        public TenantService()
+        {
+            _tenants = new List<Tenant>
+        {
+            new Tenant(1, "foo", true, "theme1"),
+            new Tenant(2, "bar", true, "theme2"),
+            new Tenant(3, "baz", false, "theme3"),
+            // Add more tenants as needed
+        };
+        }
+
+        public Tenant GetTenant(string host)
+        {
+            return _tenants.FirstOrDefault(t => t.Host.Equals(host, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+
+}
+
+
